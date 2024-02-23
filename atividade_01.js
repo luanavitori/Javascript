299869a15aa62632e807ba0ed85f8b85625f5691
luanavitori <luanavha2007@gmail.com>
2024-02-23 09:23:10 -0400

    atividade 01

---
+++ atividade_01.js
@@ -0,0 +1,13 @@
+var nome = "Luana Vitória";
+var idade = "16";
+console.log("Olá meu nome é " +nome+ " e eu tenho " +idade+ " anos.");
+
+console.log("luana".toUpperCase());
+console.log("LUANA".toLowerCase());
+
+var valor = 3;
+console.log(typeof valor);
+
+var altura = 1.59;
+var peso = 53;
+console.log(peso/altura*altura);
